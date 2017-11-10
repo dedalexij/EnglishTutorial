123abc8a3330aa7e5396c04962625af5dcb0516a
Unknown <dedalexij@mail.ru>
2017-11-10 23:46:01 +0300

    Add unit-tests

--- UnitTestProject1/UserUnitTest.cs
+++ UnitTestProject1/UserUnitTest.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using EnglishTutorial.Domain;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace UnitTestProject1
@@ -7,10 +8,64 @@ namespace UnitTestProject1
   public class UserUnitTest
   {
     [TestMethod]
-    public void TestMethod1()
+    public void AddWordToVocabulary()
     {
-
+      var nickname = "user1";
+      User user = new User(nickname);
+      var word = "hello";
+      var wordInStudy = new WordInStudy(word).Word;
+      user.AddWordToVocabulary(word);
+      var wordInVocubulary = user.Vocabulary[0].Word;
+      Assert.AreEqual(wordInStudy, wordInVocubulary);
     }
-    
+
+    [TestMethod]
+    public void RiseCountTo1()
+    {
+      var nickname = "user1";
+      User user = new User(nickname);
+      var word = "hello";
+      user.AddWordToVocabulary(word);
+      user.RiseCount(word);
+      var countInVocabulary = user.Vocabulary[0].Count;
+      var count = 1;
+      Assert.AreEqual(countInVocabulary, count);
+    }
+
+    [TestMethod]
+    public void ReduceCountTo0()
+    {
+      var nickname = "user1";
+      User user = new User(nickname);
+      var word = "hello";
+      user.AddWordToVocabulary(word);
+      user.RiseCount(word);
+      user.ReduceCount(word);
+      var countInVocabulary = user.Vocabulary[0].Count;
+      var count = 0;
+      Assert.AreEqual(countInVocabulary, count);
+    }
+    [TestMethod]
+    public void MethodExistOrNotExistMustReturnMinus1()
+    {
+      var nickname = "user1";
+      User user = new User(nickname);
+      var word = "hello";
+      var word2 = "yes";
+      user.AddWordToVocabulary(word);
+      var count = user.ExistOrNotExistWord(word2);
+      Assert.AreEqual(-1, count);
+    }
+    [TestMethod]
+    public void MethodExistOrNotExistMustReturnIndexOfWordInVocabulary()
+    {
+      var nickname = "user1";
+      User user = new User(nickname);
+      var word = "hello";
+      user.AddWordToVocabulary(word);
+      var count = user.ExistOrNotExistWord(word);
+      var wordIndex = 0;
+      Assert.AreEqual(wordIndex, count);
+    }
   }
 }
---
+++ UnitTestProject1/WordInStudyUnitTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using EnglishTutorial.Domain;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+  [TestClass]
+  public class WordInStudyUnitTest
+  {
+    [TestMethod]
+    public void RiseCountTo1()
+    {
+      var word = "hello";
+      WordInStudy wordInStudy = new WordInStudy(word);
+      wordInStudy.RiseCount();
+      var count = wordInStudy.Count;
+      var countEqual1 = 1;
+      Assert.AreEqual(countEqual1, count);
+    }
+    [TestMethod]
+    public void ReduceCountTo0()
+    {
+      var word = "hello";
+      WordInStudy wordInStudy = new WordInStudy(word);
+      wordInStudy.RiseCount();
+      wordInStudy.ReduceCount();
+      var count = wordInStudy.Count;
+      var countIsEqual0 = 0;
+      Assert.AreEqual(countIsEqual0, count);
+    }
+  }
+}
